9519b3c61a2adb0c0eb50d7c32430ad03eacedd5
B3N1ce <33745653+B3N1ce@users.noreply.github.com>
2018-05-09 17:02:29 +0200

    Add files via upload

---
+++ js/userMenuTouchControl.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function handleUserMenuClickEvent () {
+    var headerUserMenu = document.getElementByClass("header-user-menu");
+    if (headerUserMenu.clientHeight > 0){
+        alert();
+    }
+}
